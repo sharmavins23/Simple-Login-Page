159df65e1c3dd9d5902bcf0b29437c09a833b12c
Vins <sharmavins23@gmail.com>
2020-11-07 19:23:08 -0500

    ⚡️ functionality complete

--- src/index.js
+++ src/index.js
@@ -2,23 +2,26 @@
 var loginData = {};
 
 function swapForm(event) {
+    // Stop default behavior on link click
+    event.preventDefault();
+
     // Get document elements
-    registerForm = document.getElementsByClassName("register-form")[0];
-    loginForm = document.getElementsByClassName("login-form")[0];
+    let registerForm = document.getElementsByClassName("register-form")[0];
+    let loginForm = document.getElementsByClassName("login-form")[0];
 
     // Get document element ID
-    currentFormID = event.target.id;
+    let currentForm = event.target.parentNode;
 
-    // Swap based on the current ID
-    switch (currentFormID) {
-        case "loginSwap":
-            loginForm.style.display = "none";
-            registerForm.style.display = "block";
-            break;
-        case "registerSwap":
-            loginForm.style.display = "block";
-            registerForm.style.display = "none";
-            break;
+    // Swap based on parent
+    if (currentForm.className == "register-form") {
+        loginForm.style.display = "block";
+        registerForm.style.display = "none";
+    } else if (currentForm.className == "login-form") {
+        loginForm.style.display = "none";
+        registerForm.style.display = "block";
+    } else {
+        // This should not happen
+        alert("Error: Swap state failed. You should not see this message.");
     }
 }
 
@@ -26,15 +29,32 @@ function registerClicked(event) {
     // Scrape information from form
     let firstName = document.getElementById("firstname").value;
     let lastName = document.getElementById("lastname").value;
-    let school = document.getElementById("schoolname").value;
+    let schoolName = document.getElementById("schoolname").value;
 
     // Generate a username
-    let username = firstName[0] + lastName[0] + school.split(" ")[0];
+    let username = firstName[0] + lastName[0] + schoolName.split(" ")[0];
 
-    alert(username);
+    alert(`Your username to log in is ${username}`);
+
+    loginData[username] = {
+        first: firstName,
+        last: lastName,
+        school: schoolName,
+    };
+
+    swapForm(event);
 }
 
 function logOnClicked(event) {
     // Scrape information from form
     let username = document.getElementById("username").value;
+
+    if (loginData.hasOwnProperty(username)) {
+        // Save information
+        let user = loginData[username];
+
+        alert(`Welcome back, ${user.first} ${user.last}!`);
+    } else {
+        alert("Error: Invalid username.");
+    }
 }
